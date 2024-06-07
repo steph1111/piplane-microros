61ff72451f19a623ac035ae41fdd9ae8de2bdcbe
taschnell <schnellteo@hotmail.com>
2024-06-07 12:08:48 -0700

    some refinement, however program crashs due too lack of memory

--- src/piplane/piplane.c
+++ src/piplane/piplane.c
@@ -1,73 +1,53 @@
 #include <stdbool.h>
-
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
-#include <std_msgs/msg/int32.h>
 #include <std_msgs/msg/int16_multi_array.h>
 #include <rmw_microros/rmw_microros.h>
 #include <stdio.h>
-
-#include <rcl/rcl.h>
-#include <rcl/error_handling.h>
-#include <rclc/rclc.h>
-#include <rclc/executor.h>
-#include <std_msgs/msg/int32.h>
-#include <std_msgs/msg/int16_multi_array.h>
-#include <rmw_microros/rmw_microros.h>
-
 #include "pico/stdlib.h"
 #include "pico_uart_transports.h"
 #include "hardware/pio.h"
 #include "dshot.h"
 
-rcl_publisher_t publisher;
 rcl_subscription_t subscriber;
 
-// meaningful things
+// Meaningful things
 const uint GPIO[4] = {2, 3, 4, 5};
 const int NUM_MOTORS = 4;
 struct dshot_controller controller0;
+
 /**
- * Subscription call back for messages
+ * Subscription callback for messages
  */
 void subscription_callback(const void *msgin)
 {
-    // Set the msgin to a Int16MultiArray
-    // Expects, array len 4, values 0-2047
-    std_msgs__msg__Int16MultiArray *msg = (std_msgs__msg__Int16MultiArray *)msgin;
-    // set_duty_cycle((uint16_t *) msg->data.data);
+    gpio_put(15, 1);
 
-    // This might work i dunno tbh
+    // Set the msgin to an Int16MultiArray
+    // Expects array len 4, values 0-2047
+    const std_msgs__msg__Int16MultiArray *msg = (const std_msgs__msg__Int16MultiArray *)msgin;
 
     for (uint8_t i = 0; i < 4; ++i)
     {
         dshot_throttle(&controller0, i, msg->data.data[i]);
     }
-
-    dshot_loop(&controller0);
 }
 
 int main()
 {
-    // Debug LED
-    const uint LED_PIN = 15;
-
     // Initialize the LED pin
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_put(LED_PIN, 1);
-    
-    gpio_put(LED_PIN, 1);
+    gpio_init(15);
+    gpio_set_dir(15, GPIO_OUT);
+    gpio_put(15, 1);
 
     stdio_init_all();
-    /*
-    Initilizes controller 0 with DSHOT 300 on pio0, state machine 0,
-    * 4 channels starting on pin 2 */
+
+    // Initialize controller 0 with DSHOT 300 on pio0, state machine 0, 4 channels starting on pin 2
     dshot_controller_init(&controller0, 300, pio0, 0, 2, NUM_MOTORS);
 
-    // Get type support for Int16MultiArray (Change to correct type)
+    // Get type support for Int16MultiArray
     const rosidl_message_type_support_t *type_support = ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int16MultiArray);
 
     rmw_uros_set_custom_transport(
@@ -78,7 +58,6 @@ int main()
         pico_serial_transport_write,
         pico_serial_transport_read);
 
-    rcl_timer_t timer;
     rcl_node_t node;
     rcl_allocator_t allocator;
     rclc_support_t support;
@@ -94,7 +73,7 @@ int main()
 
     if (ret != RCL_RET_OK)
     {
-        gpio_put(LED_PIN, 0);
+        gpio_put(15, 0);
         return ret; // Unreachable agent, exiting program.
     }
 
@@ -102,72 +81,49 @@ int main()
 
     rclc_node_init_default(&node, "motor_array", "", &support);
 
-    // Define msg
+    // Define and initialize msg
     std_msgs__msg__Int16MultiArray msg;
+    std_msgs__msg__MultiArrayLayout layout;
+    std_msgs__msg__MultiArrayDimension layout_dim;
 
-    // Define msg data Int16 Sequence
-    rosidl_runtime_c__int16__Sequence msg_data;
-    msg_data.size = 0;
-    msg_data.capacity = 4;
-    int16_t msg_data_data[4] = {0, 0, 0, 0};
-    msg_data.data = msg_data_data;
-    msg.data = msg_data;
+    // Initialize layout_dim
+    layout_dim.label.data = "";
+    layout_dim.label.size = 0;
+    layout_dim.label.capacity = 0;
+    layout_dim.size = 4;
+    layout_dim.stride = 4;
 
-    // Define msg layout MultiArray Layout
-    // FIXME: Change to correct message type
-    std_msgs__msg__MultiArrayLayout msg_layout;
-    std_msgs__msg__MultiArrayDimension__Sequence msg_layout_dim;
-    msg_layout.dim = msg_layout_dim;
-    msg_layout.data_offset = 0;
-    msg.layout = msg_layout;
+    // Initialize layout
+    std_msgs__msg__MultiArrayDimension__Sequence layout_dim_seq;
+    layout_dim_seq.size = 1;
+    layout_dim_seq.capacity = 1;
+    layout_dim_seq.data = &layout_dim;
+
+    layout.dim = layout_dim_seq;
+    layout.data_offset = 0;
+
+    // Initialize msg data
+    msg.layout = layout;
+    int16_t data[4] = {0, 0, 0, 0};
+    msg.data.data = data;
+    msg.data.size = 4;
+    msg.data.capacity = 4;
 
     // Subscribe to topic from Pi, motor_array
-    ret = rclc_subscription_init_default(&subscriber, &node, type_support, "motor_array");
+    ret = rclc_subscription_init_default(&subscriber, &node, type_support, "/motor_array");
     rclc_executor_init(&executor, &support.context, 1, &allocator);
+
     // Choose callback for subscription
     rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA);
 
     while (true)
     {
+        gpio_put(15, 0);
+
         rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+        dshot_loop(&controller0);
     }
 
-    gpio_put(LED_PIN, 0);
+    gpio_put(15, 0);
     return 0;
 }
-
-
-
-// #include <stdio.h>
-// #include "pico/stdlib.h"
-// #include "dshot.h"
-
-// struct dshot_controller controller0;
-
-// int main(void)
-// {
-// 	int time;
-
-// 	stdio_init_all();
-
-// 	/* initialize controller 0 with DSHOT600 on pio0, state machine 0
-// 	 * with 4 channels starting on pin 2 */
-// 	dshot_controller_init(&controller0, 300, pio0, 0, 2, 4);
-
- 
-// 	while (true) {
-// 		time = to_ms_since_boot(get_absolute_time());
-// 		/* at the beginning of the first second, spin the first motor. On the second second, spin the second motor, etc */
-// 		for (int i = 0; i < 4; i++) {
-// 			if (time > (2000))
-// 				dshot_throttle(&controller0, i, 200);
-// 			else
-// 				dshot_throttle(&controller0, i, 0);
-// 		}
-
-// 		/* run the dshot loop */
-// 		dshot_loop(&controller0);
-// 	}
-
-// 	return 0;
-// }
