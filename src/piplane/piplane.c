9bac74e80339481befcffdf7eddafef099c9e285
taschnell <schnellteo@hotmail.com>
2024-06-10 10:03:25 -0700

    latest variation

--- src/piplane/piplane.c
+++ src/piplane/piplane.c
@@ -16,7 +16,6 @@ int16_t motor_throttles[4] = {0, 0, 0, 0};
 
 
 void subscription_callback(const void *msgin) {
-  gpio_put(15, 0);
 
   // Set the msgin to an Int16MultiArray
   // Expects array len 4, values 0-2047
@@ -28,10 +27,6 @@ void subscription_callback(const void *msgin) {
   }
 }
 
-#include "dshot.h"
-#include "pico/stdlib.h"
-#include <stdio.h>
-
 struct dshot_controller controller0;
 
 int main(void) {
@@ -39,7 +34,7 @@ int main(void) {
 
   /* initialize controller 0 with DSHOT300 on pio0, state machine 0
    * with 1 channel on pin 2 */
-  dshot_controller_init(&controller0, 300, pio0, 0, 2, 4);
+  dshot_controller_init(&controller0, 150, pio0, 0, 2, 4);
 
   gpio_init(15);
   gpio_set_dir(15, GPIO_OUT);
@@ -110,19 +105,19 @@ int main(void) {
   // Choose callback for subscription
   rclc_executor_add_subscription(&executor, &subscriber, &msg,
                                  &subscription_callback, ON_NEW_DATA);
-  int time;
+
   while (true) {
-    rclc_executor_spin_some(&executor, RCL_MS_TO_NS(0.001));
-    time = to_ms_since_boot(get_absolute_time());
+    rclc_executor_spin_some(&executor, RCL_MS_TO_NS(1));
+    // time = to_ms_since_boot(get_absolute_time());
 
     /* set motor throttles based on received array */
 
     for (int i = 0; i < 4; i++) {
-      if (time > 5000) {
+    //   if (time > 5000) {
         dshot_throttle(&controller0, i, motor_throttles[i]);
-      } else {
-        dshot_throttle(&controller0, i, 0);
-      }
+    //   } else {
+    //     dshot_throttle(&controller0, i, 0);
+    //   }
     }
 
     dshot_loop(&controller0);
